8736a718031f7f9e7f0246203a88ec060f95040a
Lukashyna <L_A_A_93@mail.ru>
2019-11-24 20:27:57 +0200

    change mod 01

--- Module-01/task-04.js
+++ Module-01/task-04.js
@@ -1,20 +1,17 @@
+`use strict`;
 let credits = 23580;
 let pricePerDroid = 3000;
 let totalPrice;
 let message;
 let invoice = prompt(`Количество выбранного товара для оформления заявки: `);
-invoice = Number(invoice);
+
 totalPrice = invoice * pricePerDroid;
-if (invoice < 0) {
-  console.log(`Ошибка`);
+if (invoice === null) {
+  message = `Отменено пользователем!`;
+} else if (totalPrice > credits) {
+  message = `Недостаточно средств на счету!`;
 } else {
-  if (totalPrice > credits) {
-    message = `Недостаточно средств на счету!`;
-  } else if (totalPrice <= credits) {
-    message = `Вы купили ${invoice} дроидов, на счету осталось ${credits -
-      totalPrice} кредитов.`;
-  } else {
-    message = `Отменено пользователем!`;
-  }
-  console.log(message);
+  message = `Вы купили ${invoice} дроидов, на счету осталось ${credits -
+    totalPrice} кредитов.`;
 }
+console.log(message);
